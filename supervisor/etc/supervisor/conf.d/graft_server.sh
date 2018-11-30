179b9c2770cbc163ef1ba066e0fb3084d82874da
gareth <gareth>
2018-11-30 21:39:19 +0200

    starting new branch for 2nd graft docker container on same machine

---
+++ supervisor/etc/supervisor/conf.d/graft_server.sh
@@ -0,0 +1,2 @@
+#sh script to run graft_server
+bash -c 'sleep 120 && graft_server --log-file supernode.log --log-level 1 > out.log 2>&1'
---
+++ supervisor/etc/supervisor/conf.d/watch-only-wallets.sh
@@ -0,0 +1,1 @@
+bash -c 'mkdir -p ~/.graft/supernode/data/{watch-only-wallets,stake-wallet} && cd ~/.graft/supernode/data && curl -s https://rta.graft.observer/lmdb/watch-only-wallets.tar | tar xvf -'
