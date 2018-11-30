68a806c92d461b0276c28f9f3d0bcde6ee001a89
gareth <gareth>
2018-11-30 23:09:06 +0200

    resolved issues with bash scrpts

--- supervisor/etc/supervisor/conf.d/graft_server.sh
+++ supervisor/etc/supervisor/conf.d/graft_server.sh
@@ -1,2 +1,3 @@
 #sh script to run graft_server
-bash -c 'sleep 120 && graft_server --log-file supernode.log --log-level 1 > out.log 2>&1'
+sleep 120
+graft_server --log-file supernode.log --log-level 1 > out.log 2>&1
