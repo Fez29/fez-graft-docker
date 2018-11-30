36167111305047e2a2ff842785285be4231b4dff
gareth <gareth>
2018-12-01 01:54:58 +0200

    testing sleep command

--- supervisor/etc/supervisor/conf.d/graftnoded.sh
+++ supervisor/etc/supervisor/conf.d/graftnoded.sh
@@ -2,4 +2,5 @@
 # add name of other docker container in place of "graft_graftsn-experimetal2"
 #example with DNS record
 #graftnoded --testnet --add-priority-node `getent hosts graft_graftsn-experimetal2 | awk '{print $1; exit}'`:28680 --detach
+sleep 120
 graftnoded --testnet --detach
