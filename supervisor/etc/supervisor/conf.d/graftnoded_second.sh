f1c0c1b8a75877bdeec7b270ae9d6d5cb8cea8e2
gareth <gareth>
2018-12-01 06:33:44 +0200

    added --out-peers 100 --enforce-dns-checkpointing to graftnoded commands

--- supervisor/etc/supervisor/conf.d/graftnoded_second.sh
+++ supervisor/etc/supervisor/conf.d/graftnoded_second.sh
@@ -2,4 +2,4 @@
 # add name of other docker container in place of "graft_graftsn-experimetal2"
 #example with DNS record
 #graftnoded --testnet --add-priority-node `getent hosts graft_graftsn-experimetal2 | awk '{print $1; exit}'`:28680 --detach
-graftnoded --testnet --add-priority-node `getent hosts graft_graftsn-experimetal2 | awk '{print $1; exit}'`:28680 --detach
+graftnoded --testnet --add-priority-node `getent hosts graft_graftsn-experimetal2 | awk '{print $1; exit}'`:28680 --out-peers 100 --enforce-dns-checkpointing --detach
