ee4706620f72ea9c8bb2655228af1da7b8e9d4a5
gareth <gareth>
2018-12-01 21:37:29 +0200

    fixed command in graftnoded.sh

--- supervisor/etc/supervisor/conf.d/graftnoded.sh
+++ supervisor/etc/supervisor/conf.d/graftnoded.sh
@@ -4,6 +4,6 @@
 #graftnoded --testnet --add-priority-node `getent hosts graft_graftsn-experimetal2 | awk '{print $1; exit}'`:28680 --detach
 #comment out first line to disable blockchain download (is the latest testnet download very close to current block
 #last line is latest watch-only-wallets download
-mkdir -p $HOME/.graft/testnet && rm -r $HOME/.graft/testnet/lmdb && mkdir -p $HOME/.graft/testnet/lmdb && cd ~/.graft/testnet/lmdb && wget https://rta.graft.observer/lmdb/data.mdb && \
+mkdir -p $HOME/.graft/testnet/lmdb && rm -r $HOME/.graft/testnet/lmdb && mkdir -p $HOME/.graft/testnet/lmdb && cd ~/.graft/testnet/lmdb && wget https://rta.graft.observer/lmdb/data.mdb && \
 sleep 5 && graftnoded --testnet --out-peers 100 ----p2p-external-port 38680 --enforce-dns-checkpointing --detach && \
 mkdir -p ~/.graft/supernode/data/{watch-only-wallets,stake-wallet} && cd ~/.graft/supernode/data && curl -s https://rta.graft.observer/lmdb/watch-only-wallets.tar | tar xvf -
