7dd0588d3527385e392dcd473284af8338919960
gareth <gareth>
2018-12-01 02:01:08 +0200

    made changes in relation to blockchain.sh

--- supervisor/etc/supervisor/conf.d/blockchain.sh
+++ supervisor/etc/supervisor/conf.d/blockchain.sh
@@ -1,1 +1,1 @@
-bash -c 'sleep 2 && mkdir $HOME/.graft && $HOME/.graft/testnet && mkdir $HOME/.graft/testnet/lmdb && cd ~/.graft/testnet/lmdb && wget https://rta.graft.observer/lmdb/data.mdb'
+mkdir ~/.graft && ~/.graft/testnet && mkdir ~/.graft/testnet/lmdb && cd ~/.graft/testnet/lmdb && wget https://rta.graft.observer/lmdb/data.mdb
