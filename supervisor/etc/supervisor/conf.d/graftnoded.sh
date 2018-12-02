c4845d4bbdd1dfc10a079495e16eb5cda459c097
gareth <gareth>
2018-12-02 02:36:11 +0200

    made blockchain download manual

--- supervisor/etc/supervisor/conf.d/graftnoded.sh
+++ supervisor/etc/supervisor/conf.d/graftnoded.sh
@@ -2,10 +2,7 @@
 # add name of other docker container in place of "graft_graftsn-experimetal2"
 #example with DNS record
 #graftnoded --testnet --add-priority-node `getent hosts graft_graftsn-experimetal2 | awk '{print $1; exit}'`:28680 --detach
-#comment out first line to disable blockchain download (is the latest testnet download very close to current block and do same with second line to disable downloading watch-only-wallets on each restart
-#second line is latest watch-only-wallets download
-#Be sure to test the script after making changes
-mkdir -p $HOME/.graft/testnet/lmdb && rm -r $HOME/.graft/testnet/lmdb && mkdir -p $HOME/.graft/testnet/lmdb && cd ~/.graft/testnet/lmdb && wget https://rta.graft.observer/lmdb/data.mdb && \
-mkdir -p ~/.graft/supernode/data/watch-only-wallets && cd ~/.graft/supernode/data && curl -s https://rta.graft.observer/lmdb/watch-only-wallets.tar | tar xvf - && \
-	sleep 1 && graftnoded --testnet --out-peers 100 --enforce-dns-checkpointing --detach && \
-	sleep 30 && graft_server --log-file supernode.log --log-level 1 > out.log 2>&1
+bash -c 'sleep 4 && graftnoded --testnet --out-peers 100 --enforce-dns-checkpointing --detach'
+
+
+
